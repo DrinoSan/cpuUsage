ddb44ed4f5531ca2e845df75c7c5d0a06798e832
DrinoSan <s.becirbegovic@gmail.com>
2023-11-26 00:18:13 +0100

    passing props to preact

--- src/index.mjs
+++ src/index.mjs
@@ -1,19 +1,29 @@
 import { h, render } from 'https://esm.sh/preact';
+import htm from 'https://esm.sh/htm';
+
+const html = htm.bind(h);
+
+function App(props) {
+    return html`
+    <div>
+    ${props.cpus.map((cpu) => {
+        return html`
+        <div> ${cpu.toFixed(2)}% usage </div> `;
+    })}
+    </div>
+    `;
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     setInterval(async () => {
         let response = await fetch('/api/cpus');
-        if( response.status !== 200 ) {
+        if (response.status !== 200) {
             throw new Error('Http error! status: ${response.status}');
         }
 
         let json = await response.json();
 
-        // document.body.textContent = JSON.stringify(json, null, 2);
-
-        const app = h('pre', null, JSON.stringify(json, null, 2));
-
-        render(app, document.body);
+        render(html`<${App} cpus=${json}></${App}>`, document.body);
     }, 1000);
 
 });
