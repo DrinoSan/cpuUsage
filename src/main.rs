da41bc84a5d9aea78351f083ddb1b3bbc4ee9ddc
DrinoSan <s.becirbegovic@gmail.com>
2023-11-25 23:22:24 +0100

    feature: Added sysinfo to log cpu usage on request

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
-use axum::{routing::get, Router};
+use std::sync::{Arc, Mutex};
+
+use axum::{routing::get, Router, extract::State};
+
+use sysinfo::{CpuExt, System, SystemExt};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello World!" }));
+    let app = Router::new()
+        .route("/", get(|| async { "Hello World!" }))
+        .route("/cpu", get(root_get))
+        .with_state(AppState {
+            sys: Arc::new(Mutex::new(System::new())),
+        });
 
     let server =
         axum::Server::bind(&"0.0.0.0:8080".parse().unwrap()).serve(app.into_make_service());
@@ -11,4 +20,27 @@ async fn main() {
     println!("Listening on address. {}", addr);
 
     server.await.unwrap();
+}
+
+#[derive(Clone)]
+struct AppState {
+    sys: Arc<Mutex<System>>,
+}
+
+async fn root_get(State(state): State<AppState>) -> String {
+    use std::fmt::Write;
+
+    let mut s = String::new();
+
+    let mut sys = state.sys.lock().unwrap();
+    sys.refresh_all();
+
+    for (i, cpu) in sys.cpus().iter().enumerate() {
+        let i = i + 1;
+
+        let usage = cpu.cpu_usage();
+        writeln!(&mut s, "CPU {i} {usage}% ").unwrap();
+    }
+
+    s
 }
